5e2ff1e64891ddce5a2db38b04149e2625e922f9
João Ferreira <joaof@invine.com>
2016-10-27 15:57:05 +0100

    Algorithms - Implementation - Easy - Divisible Sum Pairs

---
+++ src/algorithms/implementation/easy/_03DivisibleSumPairs/Solution.java
@@ -0,0 +1,29 @@
+package algorithms.implementation.easy._03DivisibleSumPairs;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int []a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        int results = 0;
+        for (int i = 0; i < n ; i++) {
+            for (int j = i + 1; j < n ; j++) {
+                if ((a[i] + a[j]) % k == 0) {
+                    results++;
+                }
+            }
+        }
+
+        sc.close();
+        System.out.println(results);
+    }
+}
