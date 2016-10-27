8759434f388dcfe33d5ff155caf7e220dc68c5dc
João Ferreira <joaof@invine.com>
2016-10-27 17:48:06 +0100

    Algorithms - Implementation - Easy - Sock Merchant

---
+++ src/algorithms/implementation/easy/_06SockMerchant/Solution.java
@@ -0,0 +1,28 @@
+package algorithms.implementation.easy._06SockMerchant;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int sock, matches = 0, sock_index;
+		ArrayList<Integer> colors = new ArrayList<>();
+		
+		for (int i = 0; i < n; i++) {
+			sock = sc.nextInt();
+			sock_index = colors.indexOf(sock);
+			if (sock_index != -1) {
+				colors.remove(sock_index);
+				matches++;
+			} else {
+				colors.add(sock);
+			}
+		}
+		
+		System.out.println(matches);
+		sc.close();
+	}
+}
