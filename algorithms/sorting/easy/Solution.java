5fc9511d86598eba3da18bb0ae40d7a990e1afc9
João Ferreira <jooaooferreira@gmail.com>
2018-01-22 19:58:26 +0000

    changed schema

--- algorithms/sorting/easy/Solution.java
+++ algorithms/sorting/easy/Solution.java
@@ -2,24 +2,22 @@ package algorithms.sorting.easy;
 
 import java.util.Scanner;
 
-public class IntroToTutorialChallenges {
-	
+public class Solution {
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
+		int temp;
 		int V = sc.nextInt();
 		int n = sc.nextInt();
-		int temp;
-		
+
 		for (int i = 0; i < n; i++) {
 			temp = sc.nextInt();
-			
 			if (V == temp) {
 				System.out.println(i);
 				break;
 			}
 		}
-		
 		sc.close();
 	}
 
